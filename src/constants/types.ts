5e49eba52b11257f86b25d4c897867e830624046
Lan Anh <dhlananh2309@gmail.com>
2025-06-27 22:15:05 +0700

    feat: add PathItem and InputSearchProps types

--- src/constants/types.ts
+++ src/constants/types.ts
@@ -1,1 +1,13 @@
-// import { ChangeEvent } from "react";
+import type { ChangeEvent } from "react";
+
+export interface PathItem {
+	[k: string]: {
+		PATH: string;
+		LABEL: string;
+		BREADCRUMB: Array<string>;
+	};
+}
+
+export interface InputSearchProps {
+	handleSearchChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}
